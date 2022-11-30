59a0acbf5ba973668c11572c6c0aa8dbb53b7f70
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-11-30 22:32:17 +0100

    Refactor main file.

--- reciprocals_of_numbers.py
+++ reciprocals_of_numbers.py
@@ -1,4 +1,4 @@
-def calculate_decimal_expansion(number: int, print_short_description: bool=True):
+def calculate_decimal_expansion(number: int, print_short_description: bool = True):
     if number < 2:
         raise ValueError("Enter number greater than 1!")
     decimal_expansion_list = []
@@ -11,8 +11,8 @@ def calculate_decimal_expansion(number: int, print_short_description: bool=True)
         whole_division_product = current_remainder // number
         current_remainder %= number
         if whole_division_product in decimal_expansion_list:
-            indeces = [i for i, x in enumerate(decimal_expansion_list) if x == whole_division_product]
-            for i in indeces:
+            indices = [i for i, x in enumerate(decimal_expansion_list) if x == whole_division_product]
+            for i in indices:
                 if remainder_list[i] == current_remainder and current_remainder != 0:
                     period_index = i
                     break
@@ -25,13 +25,13 @@ def calculate_decimal_expansion(number: int, print_short_description: bool=True)
         remainder_list.append(current_remainder)
     if current_remainder == 0:
         if print_short_description:
-            print("Reciprocal of " + str(number) + " has no period.")
-            print("1/" + str(number) + " = ", end="")
+            print(f"Reciprocal of {number} has no period.")
+            print(f"1/{number} = ", end="")
         print("0.", end="")
     else:
         if print_short_description:
-            print("Number of digits in a period of decinal expansion of " + str(number) + ": " + str(len(decimal_expansion_list)))
-            print("Decimal expansion of 1/" + str(number) + ": ", end="")
+            print(f"Number of digits in a period of decimal expansion of {number}: {len(decimal_expansion_list)}")
+            print(f"Decimal expansion of 1/{number}: ", end="")
         print("0.", end="")
     for index, number in enumerate(decimal_expansion_list):
         if index == period_index:
@@ -45,7 +45,7 @@ def calculate_decimal_expansion(number: int, print_short_description: bool=True)
 
 
 if __name__ == "__main__":
-    for i in range(2, 100):
-        calculate_decimal_expansion(i, True)
-    #Line of code below currently takes more than 140s to compute
+    for number in range(2, 100):
+        calculate_decimal_expansion(number, print_short_description=True)
+    # Line of code below currently takes more than 140s to compute
     calculate_decimal_expansion(60017)
